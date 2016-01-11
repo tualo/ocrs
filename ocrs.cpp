c6b6dec4ec88b34e15aa18552ae31fddf5b21fa8
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2016-01-11 11:16:39 +0100

    accept single file

--- ocrs.cpp
+++ ocrs.cpp
@@ -1,30 +1,28 @@
 #include "ImageRecognize.h"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <pthread.h>
-#include <glob.h>
 #include <vector>
 #include <string>
 
-int MAX_FILENAME_LENGTH=255;
+#include "glob.h"
 
 struct thread_info {
-  int block_id;
-  int loop;
-  unsigned char data[MAX_FILENAME_LENGTH];
+  int index;
 };
+typedef struct thread_info thread_info;
 
+std::vector<std::string> files;
+glob::Glob* gl;
 
-inline std::vector<std::string> glob(const std::string& pat){
-  using namespace std;
-  glob_t glob_result;
-  glob(pat.c_str(),GLOB_TILDE,NULL,&glob_result);
-  vector<string> ret;
-  for(unsigned int i=0;i<glob_result.gl_pathc;++i){
-      ret.push_back(string(glob_result.gl_pathv[i]));
-  }
-  globfree(&glob_result);
-  return ret;
+void* processImage(void *data ){
+  struct thread_info *tib;
+  tib = (struct thread_info *)data;
+
+  pthread_exit((void*)42);
+	return 0;
 }
+
+
 
 /**
  * @function main
@@ -34,17 +32,41 @@ int main( int argc, char** argv )
   double t = (double)cv::getTickCount();
 
 
-  ImageRecognize* ir = new ImageRecognize();
-  ir->open(argv[1]);
 
+  gl=new glob::Glob(argv[1]);
+  if (!gl->hasNext()){
+    ImageRecognize* ir = new ImageRecognize();
+    ir->open(argv[1]);
+  }else{
+    while (gl->hasNext()) {
+      std::cout << gl->GetFileName() << std::endl;
+      gl->Next();
+      ImageRecognize* ir = new ImageRecognize();
+      std::stringstream str;
+      str << argv[1] << "/" << gl->GetFileName();
+      std::string filename = str.str();
+      char *fn = new char[filename.length() + 1];
+      strcpy(fn, filename.c_str());
+      ir->open(fn);
+      delete[] fn;
+    }
+  }
+
+  //ImageRecognize* ir = new ImageRecognize();
+  //ir->open(argv[1]);
+
+  //filename
+  //const unsigned char * constStr =
+  /*
+  thread_info packet_info = {0,reinterpret_cast<const unsigned char *> (strVar.c_str())};
   pthread_t thread;
-   int ct = pthread_create( &thread, NULL, writePacket, (void*)packet_info);
-   if( ct  != 0) {
-     printf("something went wrong while threading %i\n",ct);
-     return 0;
-   }
-   pthread_join(thread,NULL);
-
+  int ct = pthread_create( &thread, NULL, processImage, (void*)packet_info);
+  if( ct  != 0) {
+   printf("something went wrong while threading %i\n",ct);
+   return 0;
+  }
+  pthread_join(thread,NULL);
+  */
   t = ((double)cv::getTickCount() - t)/cv::getTickFrequency();
   std::cout << "Times passed in seconds: " << t << std::endl;
   return -1;
