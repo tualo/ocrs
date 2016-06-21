976e28ec8b1cf29fce5318572aee640efede43ab
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2016-06-21 10:09:49 +0200

    fixed analyse

--- analyse.sh
+++ analyse.sh
@@ -9,13 +9,13 @@ export MODELL=Clearing
 export USEPZA=0
 export MANDANT=0000
 
-if [[ $# -eq 0 ]] ; then
-  export X=1
-else
-  export IMAGEPATH=$1
-  export STORE_ERRORS=$1error/
-  export STORE_ORIGINAL=$1original/
-fi
+#if [[ $# -eq 0 ]] ; then
+#  export X=1
+#else
+#  export IMAGEPATH=$1
+#  export STORE_ERRORS=$1error/
+#  export STORE_ORIGINAL=$1original/
+#fi
 echo "checking $IMAGEPATH";
 while :
 do
