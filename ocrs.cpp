b8a31879b4924a66d9d851c4637d07f61021e2a4
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2016-06-23 14:21:18 +0200

    swapped vars

--- ocrs.cpp
+++ ocrs.cpp
@@ -159,8 +159,8 @@ int main( int argc, char** argv ){
     ir->openPZA(argv[1]);
   }else{
     ir->cmWidth =  std::atoi(width);
-    std::cout << "WIDTH " << scale << std::endl;
-    std::cout << "SCALE " << width << std::endl;
+    std::cout << "WIDTH " << width << std::endl;
+    std::cout << "SCALE " << scale << std::endl;
     ir->scale = std::atof(scale);
     ir->open(argv[1]);
   }
