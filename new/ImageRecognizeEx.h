e0e631c8122165a491a9fcee635d0afe5efaf3c4
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2017-08-31 22:39:48 +0200

    remove define

--- new/ImageRecognizeEx.h
+++ new/ImageRecognizeEx.h
@@ -2,10 +2,8 @@
 #ifndef IMAGERECOGNIZEEX_H
 #define IMAGERECOGNIZEEX_H
 
-#define int64 opencv_broken_int
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
-#undef int64
 //#include "opencv2/imgproc/imgproc.hpp"
 //#include "opencv2/imgcodecs.hpp"
 
