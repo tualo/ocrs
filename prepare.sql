7842d833a264b5fad8cc6e2e05e95c3f04603811
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2017-10-02 15:03:59 +0200

    uncomment pseude zipcode

--- prepare.sql
+++ prepare.sql
@@ -15,4 +15,11 @@ insert into ocrhash_complex (id,date_added,strasse,plz,ort,adr)
   from plz_orte where gesondert=0
 on duplicate key update ort=values(ort);
 
+
 create fulltext index id_ft_hash_complex on ocrhash_complex(adr);
+
+
+--  insert into ocrhash_complex (id,date_added,strasse,plz,ort,adr)
+--    select concat('PSEUDO',plz,ort) id, UNIX_TIMESTAMP(now()),'' strasse,plz,ort, concat(plz,' ',ort) adr
+--    from PSEUDO_PLZ
+--  on duplicate key update ort=values(ort);
